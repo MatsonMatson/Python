d1df6fadfab31ebb156d30b7385623b80d2e08c1
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-26 11:04:38 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,10 @@
+n1 = int(input('Um Valor: '))
+n2 = int(input('Outro Valor: '))
+so = n1 + n2
+su = n1 - n2
+mu = n1 * n2
+di = n1 / n2
+dii = n1 // n2
+ex = n1 ** n2
+print('A Soma é {}\n a Subtração é {}\n o Produto é {}\n e a Divisão {:.3}'.format(so, su, mu, di))
+print('A Divisão inteira é {}\n e a Potência é {}' .format(dii, ex))
