62cb0bd23f750febd539ebb3f9dbd14fb60052c6
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-26 11:05:02 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um Número: '))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1**(1/2)
+print('O Dobro de {} Vale {}' .format(n1, dobro))
+print('O Triplo de {} Vale {}' .format(n1, triplo))
+print('A Raiz Quadrada de {} Vale {:.2f}' .format(n1, raiz)) #{:.2f} ele limita as casa decimais em ponto flutuante
