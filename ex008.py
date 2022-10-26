227e4500c3126a614ff211a1bf0849b09c4cf4c9
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-26 11:05:20 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,4 @@
+medida = float(input('Digite o Metro: '))
+cent = medida * 100
+mili = medida * 1000
+print('A Medida é {}m \no Centímetros é {}cm \ne os Milímetros {}mm' .format(medida, cent, mili))
