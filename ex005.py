d6aa0fb1c5ab78502b68f80dfcf0ca4118b9fbf0
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-26 11:04:52 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um Número: '))
+#suc = n1 + 1
+#ante = n1 - 1
+print('Seu Número é {}\n o Sucessor é {}\n e o Antecessor é {}' .format(n1, (n1+1), (n1-1)))
+#foi colocado mascara no format para evitar uso de variavel
