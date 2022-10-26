00c9c88d699f84dfbf30c5d5317642293e1b37cc
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-26 11:05:10 -0300

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Digite sua Nota: '))
+nota2 = float(input('Digite sua outra Nota: '))
+media = (nota1 + nota2)/2
+print('A Média entre {} e {} é igual á {}' .format(nota1, nota2, media))
