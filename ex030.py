4dc6ad1ff05ff77b2e33b392ada7521448a7c9a9
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:05:16 -0300

    Add files via upload

---
+++ ex030.py
@@ -0,0 +1,11 @@
+import emoji
+
+
+num = int(input('Me diga um Número qualquer: '))
+
+if num % 2:
+ print('Este Número é Impar!')
+else:
+    print('Este Número é Par')
+print('Gostaria de ver outro Número?')
+print(emoji.emojize(':fox:'))
