40b6319a2f0080106a8bbee64afd685e0383cfa4
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:03:54 -0300

    Add files via upload

---
+++ ex023.py
@@ -0,0 +1,16 @@
+import emoji
+
+num = int(input('Informe um Número: '))
+
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print('Analisando o Número {}' .format(num))
+print('Unidade: {}' .format(u))
+print('Dezena: {}' .format(d))
+print('Centena: {}' .format(c))
+print('Milhar: {}' .format(m))
+
+print(emoji.emojize(':fox:'))
