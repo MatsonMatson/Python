ffbbc58ca756aa6dcd2f3c03b745b318c3559039
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:02:37 -0300

    Add files via upload

---
+++ ex016.py
@@ -0,0 +1,7 @@
+import emoji
+import math
+
+num = float(input('Digite um Número: '))
+trunc = math.trunc(num)
+print('Seu Número é {} e a parte inteira dele é {}' .format(num, trunc))
+print((emoji.emojize(":fox:")))
