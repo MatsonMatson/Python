11b9a6baf19d088a8acb448b30fbe54ffaaf8d99
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:08:16 -0300

    Add files via upload

---
+++ ex046.py
@@ -0,0 +1,8 @@
+import emoji
+from time import sleep
+
+for c in range (10, -1, -1):
+    print(c)
+    sleep(1)
+print('AEEEE, FELIZ ANO NOVOOO!', emoji.emojize(':fireworks:'':fireworks:'':fireworks:'))
+print(emoji.emojize(':fox:'))
