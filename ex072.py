a3d1db8e8f8952cab6130793971abf95bdc86489
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:15:00 -0300

    Add files via upload

---
+++ ex072.py
@@ -0,0 +1,15 @@
+import emoji
+
+cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco',
+            'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
+            'Onze', 'Doze', 'Treze', 'Quatorze/Catorze', 'Quinze',
+            'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente Novamente: ', end=' ')
+print(f'Você digitou o Número {cont[num]}')
+print(emoji.emojize(':fox:'))
+
