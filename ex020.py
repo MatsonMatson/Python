f35fe178e2ac20faff8f9f88317fcc1cad2daea5
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:03:19 -0300

    Add files via upload

---
+++ ex020.py
@@ -0,0 +1,13 @@
+import emoji
+import math
+import random
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('Terceiro Aluno: '))
+n4 = str(input('Quarto Aluno: '))
+
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+
+print('A ordem de apresentação será: {}' .format(lista))
+print(emoji.emojize(":fox:"))
