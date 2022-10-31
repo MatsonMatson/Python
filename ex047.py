7edb5c891f114990bb0dd27b974b04e7859a2f59
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-10-31 11:08:24 -0300

    Add files via upload

---
+++ ex047.py
@@ -0,0 +1,9 @@
+import emoji
+from time import sleep
+
+for c in range(0, 51, 2):
+    #if c % 2 ==: outro método de fazer
+    print(c, 'PAR') #se colocar end='' ele deixa todos do lado um do outro
+    sleep(0.5)
+print('Todos os números acima são PAR!')
+print(emoji.emojize(':fox:'))
