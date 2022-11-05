e7d2a94c1dc8bf54e05c9cb89ea5d4d3fe74b72d
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-05 18:20:48 -0300

    Add files via upload

---
+++ ex105.py
@@ -0,0 +1,22 @@
+import emoji
+
+
+def nota(*n, sit=False):
+    r = dict()
+    r ['total'] = len(n)
+    r ['maior'] = max(n)
+    r ['menor'] = min(n)
+    r ['média'] = sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+resp = float(input('Digite sua Nota: '))
+print(nota(resp))
+print(emoji.emojize(':fox:'))
