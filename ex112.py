570cb5c97a16a89cdc3f23e0d1279d9aed74b1ec
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-10 10:34:55 -0300

    Add files via upload

---
+++ ex112.py
@@ -0,0 +1,7 @@
+from Uteis import Dados
+import emoji
+
+n1 = int(input('Digite um Número Inteiro: '))
+n2 = float(input('Digite um Número Real: '))
+print(f'O valor Inteiro digitado foi {n1}, e o valor Real foi {n2}')
+print(emoji.emojize(':fox:'))
